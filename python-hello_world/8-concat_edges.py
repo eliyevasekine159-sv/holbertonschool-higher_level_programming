5e1fa1b5b082035422988f499d01a5c667450c73
Sakina <eliyevasekine159@example.com>
2026-04-25 13:17:43 +0000

    Fix pycodestyle: line too long

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented "
+str += "programming language that combines remarkable power with "
+str += "very clear syntax"
 str = str[39:67] + str[107:112] + str[:6]
 print(str)
