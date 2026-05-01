4941b1c935c6a40847fa719561691048e07ca917
Sakina <eliyevasekine159@example.com>
2026-05-01 21:19:10 +0000

    Fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0
+    nb_print = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            n += 1
+            nb_print += 1
         except IndexError:
             break
     print("")
-    return n
+    return nb_print
