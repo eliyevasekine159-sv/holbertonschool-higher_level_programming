618bbbb05b52030829db8a4f435e0a7dbe2bb68c
Sakina <eliyevasekine159@example.com>
2026-05-01 21:10:32 +0000

    Fixing task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        while i < x:
+    nb_print = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
-            i += 1
-    except IndexError:
-        pass
+            nb_print += 1
+        except IndexError:
+            break
     print("")
-    return i
+    return nb_print
