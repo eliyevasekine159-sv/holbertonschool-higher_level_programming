b55534fb2ab450fca129a4ea539dd939fde612c9
eliyevasekine159-sv <eliyevasekine159@gmail.com>
2026-05-20 22:44:07 +0000

    Fix: Resolved pycodestyle E501 and W293 style formatting issues

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email, sends a POST request with the email as parameter"""
+"""Takes URL and email, sends a POST request with the email"""
 import sys
 import urllib.parse
 import urllib.request
@@ -8,11 +8,10 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    # Parametri lüğət şəklində hazırlayırıq və urlencode ilə kodlaşdırırıq
+
     values = {'email': email}
     data = urllib.parse.urlencode(values).encode('utf-8')
-    
+
     req = urllib.request.Request(url, data=data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
