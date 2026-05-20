bee2c9945a796c6e32fad0e8d685ac294c6bf2a4
eliyevasekine159-sv <eliyevasekine159@gmail.com>
2026-05-20 23:06:08 +0000

    Task 1: Implemented Flask routes and dynamic Jinja templates with include structure

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A simple Flask application serving HTML pages using Jinja templates.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Renders the index page"""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Renders the about page"""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page"""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
